dce8e665b8907b31b85a2dc6f7e11cdf97e2f963
Sayjel Patel <sayjel.v.patel@gmail.com>
2019-09-01 07:04:28 +0400

    updates - sp

--- www/examples/sollewitt.js
+++ www/examples/sollewitt.js
@@ -1,52 +1,40 @@
-// Sayjel Vijay Patel
-// @djclds
-// DIDI MuW 2019, Lab 1, August 16, 2019
-// Wall Drawing #16 (1969)
-// Bands of lines 12 inches (30 cm) wide, in three directions (vertical, horizontal, diagonal right) intersecting.
-
-
 console.log('solvingsol.js')
 
+// Sayjel Vijay Patel, September 1, 2019 
+// Github: @djclds
+// "Sol Lewitt Drips"
 
+let w;
+let drips = []
 
-var attempt2 = function( p ) {
-
-  let w;
-  let drips = []
-
-  p.setup = function(){
+function setup(){
 
     const canvasDiv = document.getElementById('INTRO');
-    const canvasWidth = canvasDiv.offsetWidth;
-    // console.log(canvasDiv.off)
-    p.createCanvas(canvasWidth, screen.height).parent('INTRO')
-    p.noStroke()
+    createCanvas(canvasDiv.offsetWidth, screen.height).parent('INTRO')
+    noStroke()
     w = new Walker();
 
-  }
+}
 
-  p.draw = function(){
+function draw(){
 
-    w.step();
-    w.display();
-    
+  w.step();
+  w.display();
 
-  }
+}
 
-  class Walker {
+class Walker {
 
-    constructor() {
+  constructor() {
 
-      p.fill(0)
+      fill(0)
 
       this.col = [[255,0,0],[255,255,0],[0,0,255]]
 
       this.arr = []
       this.arrPrev = null
       this.count = 0
-      // this.x = width / 2;
       this.iy = 0
-      // console.log(this.iy)
       this.x = 0
       this.y = 0
       this.tx = 0;
@@ -58,11 +46,8 @@ var attempt2 = function( p ) {
 
     step() {
       
-      // noise() gir samme verdi (mellom 0 og 1) med samme parameter, men ulik verdi for hver kjøring
-      // print(noise(0)); // Aktiver denne og se selv
-      // this.x = p.map(p.noise(this.tx), 0, 1, 0, width);
-      this.x += 1
-      this.ny = p.map(p.noise(this.ty), 0, 1, 0, this.range);
+      this.x ++
+      this.ny = map(noise(this.ty), 0, 1, 0, this.range);
 
 
       if (this.arrPrev){
@@ -87,57 +72,56 @@ var attempt2 = function( p ) {
 
       this.count++
 
-      if (this.x > p.width){
+      if (this.x > width){
 
         console.log('x greather than width')
         this.x = 0;
         this.iy += 3
         this.y = this.iy 
-        // p.fill(random(255),random(255),random(255))
+        // fill(random(255),random(255),random(255))
         this.count = 0 
         this.arrPrev = [...this.arr]
         this.arr = []
         this.range = 25
 
         this.color = this.col.shift()
-        console.log("this.color",this.color,this.col)
-        p.fill(this.color[0],this.color[1],this.color[2])
+
+        fill(this.color[0],this.color[1],this.color[2])
         this.col.push(this.color)
 
       }
 
 
-      if (p.random()>0.99){
+      if (random()>0.99){
       
         let x = this.x
         let y = this.y
         let r = 5 
         drips.push([x,y,r,...this.color])
-        if (p.random()>0.5){
-          drips.shift(drips.length*p.random()*Math.floor(),1)
+        if (random()>0.5){
+          drips.shift(drips.length*random()*Math.floor(),1)
         }
       
       }
-      // console.log('step', this.iy)
+
 
     }
 
     display() {
 
-      // p.fill(255,0,0);
-              p.fill(this.color)
-      p.ellipse(this.x, this.y,3, 3);
+
+              fill(this.color)
+      ellipse(this.x, this.y,3, 3);
       drips.forEach(arr=>{
         arr[1] = arr[1] + 0.25
-        arr[0] = arr[0]+(p.mouseX-arr[0])*0.00001
+        arr[0] = arr[0]+(mouseX-arr[0])*0.00001
         arr[3] = arr[3] - 1 
-        p.fill(arr[3],arr[4],arr[5])
-        p.ellipse(arr[0],arr[1],arr[2])
+        fill(arr[3],arr[4],arr[5])
+        ellipse(arr[0],arr[1],arr[2])
 
       })
 
-      p.fill(this.color)
-      // console.log('display',this.x,this.y)
+      fill(this.color)
 
     }
 
@@ -146,7 +130,3 @@ var attempt2 = function( p ) {
 
 
 
-}
-
-
-var myp5 = new p5(attempt2);
