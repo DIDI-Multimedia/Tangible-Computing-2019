50055615b01a9a39e82874f0d413dd4515c7d142
Sayjel Patel <sayjel.v.patel@gmail.com>
2019-09-06 23:39:35 +0400

    updates

--- students/sayjel_patel/experiments/2_5/sketch.js
+++ students/sayjel_patel/experiments/2_5/sketch.js
@@ -1,30 +1,125 @@
-/*
+// Week Two, Day Four - for MuW201 Tangible Computing, 2019
+// Sayje Patel 
+// DJCLDS
+// Date 
 
-Gradient Array
+console.log('sketch.js')
 
-*/
-
+let blocks =[]
+let reset = true
 
 function setup(){
 
-  createCanvas(800,800);
-  stroke(255)
+	let canvas = createCanvas(windowWidth/3,windowWidth/3)
+	canvas.parent('sketch-holder');
+	blocks = initialize()
+	fill(255,0,0)
 
 }
+
 function draw(){
 
-  let numCol = 10
-  let numRow = 10
-  let stepX = width / numCol // height of box 
-  let stepY = height / numRow // width of box 
-   
-  background(255)
+	background(253,253,150)
+	// ellipse(width/2,height/2,500,500)
+	text(second(),50,50)
+	
+	for (var i = 0; i< blocks.length; i++){
 
-  for (var col = 0; col < numRow; col++){
-    for (var row = 0; row < numCol; row++){
-      fill(col/numCol*255,row/numRow*255,mouseX/width*255)
-      rect(row*stepX,col*stepY,stepX,stepY) 
-    }
-  }
+		updateBlock(blocks[i])
+		drawBlock(blocks[i])
+
+	}
+
+	if (second() == 59){
+		if (reset){
+		fill(random(255),random(255),random(255))
+		blocks.forEach(block=>{
+			block.tarX = random(width/2)
+				block.tarY = random(0,height/8) // (height/60)*block.id
+		
+			// block.tarY = random(height/2,height) // (height/60)*block.id
+			block.tarR = random(TWO_PI)
+		})
+		}
+		reset = false
+	}
+	if (second() == 0 && !reset){
+		reset = true
+	}
+
+}
+
+function updateBlock(block){
+	if (block.id == second()){
+		block.tarX = width - width/4
+		block.tarY = (height/60)*block.id
+		block.tarR = 0
+	}
+
+	block.posX =lerp(block.posX, block.tarX, .025)
+	block.posY =lerp(block.posY, block.tarY, .05)
+	block.rot = lerp(block.rot,block.tarR,.02)
+}
+
+function drawBlock(block){
+
+	push()
+	let x =block.posX+(60-(60-block.id)/2)
+	let y = height-block.posY
+	translate(x,y)
+	rotate(block.rot)
+	rect(0,0,(60-block.id),block.size)
+	pop()
+	stroke(0)
+	if (abs(block.rot)>0.01 ){
+		line(width/60000*millis(),0,x,y)
+	}
+
+
+
+}
+
+function initialize(){
+
+	let arr = []
+
+	for (var i =59; i > 0;i--){
+
+		let block = {}
+		block.id = i
+		block.size = height/60
+		block.posY = (height/60)*i
+		block.posX = 0 
+		block.tarX = width-width/4
+		block.tarY =(height/60)*i
+		block.rot = 0
+		block.tarR = 0
+		if (second()<block.id){
+			block.tarX = random(width/2)
+			block.tarY = random(0,height/8) // (height/60)*block.id
+			block.tarR = random(TWO_PI)
+		}
+
+		arr.push(block)
+
+	}
+
+	return arr
+
+}
+
+function move(crtr){
+
+	crtr.posX += random(-2,2)
+	crtr.posY += random(-2,2)
+
+}
+
+function display(crtr){
+
+	fill(crtr.color)
+	textSize(crtr.size)
+	text(crtr.name,crtr.posX,crtr.posY)
+	// ellipse(crtr.posX,crtr.posY,crtr.size,crtr.size)
 
 }
