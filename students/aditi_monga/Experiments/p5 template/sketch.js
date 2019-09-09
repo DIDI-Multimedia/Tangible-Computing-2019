08d37c520d2d12b25435411804dad30c13607c1f
Aditi <54794670+aditimonga@users.noreply.github.com>
2019-09-09 10:54:39 +0400

    eh

---
+++ students/aditi_monga/Experiments/p5 template/sketch.js
@@ -0,0 +1,20 @@
+// Sketch Title - for MuW201 Tangible Computing, 2019
+// Firstname Lastname 
+// GitHub Username
+// Date 
+
+function setup(){
+
+	let canvas = createCanvas(windowWidth/4,windowWidth/4)
+	
+	// Move the canvas so it's inside our <div id="sketch-holder">.
+	canvas.parent('sketch-holder');
+	background(34,155,215)
+
+
+}
+
+function draw(){
+
+
+}
