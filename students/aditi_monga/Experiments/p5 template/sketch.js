fb8ca44649ae2b665ff535ee571b3adcf1ea7212
Aditi <54794670+aditimonga@users.noreply.github.com>
2019-09-09 12:29:16 +0400

    Update sketch.js

--- students/aditi_monga/Experiments/p5 template/sketch.js
+++ students/aditi_monga/Experiments/p5 template/sketch.js
@@ -9,34 +9,40 @@ function setup(){
 	
 	// Move the canvas so it's inside our <div id="sketch-holder">.
 	canvas.parent('sketch-holder');
-
-
-
+background(random(255),155,215)
 	// text(x,x*step,height/2)
 	// x++
 	// text(x,x*step,height/2)
 	// x++
 	// text(x,x*step,height/2)
-
-}
-
-function draw(){
-
-	background(mouseX,mouseY,215)
 	var numElements = 3
-	textSize(20)
+	textSize(10)
 	var step = width/numElements
+	textAlign(CENTER,CENTER)
+	console.log(step)
 
-
-
-	// var x = 0
-	
 	for (var x = 0 ; x < numElements; x++){
 		for (var y = 0 ; y < numElements; y++){
+			fill(random(255), random(255),random(255))
+			rect(x*step,y*step,step,step)
+			fill(0)
 		var string = x + "," + y
-		text(string,x*step+40,y*step+40)
-		// fill(random(255), random(255), random(255))
-		// ellipse(mouseX,mouseY/2,step/2,step/2)
+		text(string,x*step+25,y*step+25)
 }
 }
 }
+// function draw(){
+
+// 	// background(mouseX,mouseY,215)
+
+
+
+
+// 	// var x = 0
+	
+	
+// 		// fill(random(255), random(255), random(255))
+// 		// ellipse(mouseX,mouseY/2,step/2,step/2)
+// }
+// }
+// }
