17ae7ffecb51e0df1c012d9f5afe22a9b553ffde
hoor bakhit <hoorkhamis2001@hotmail.com>
2019-09-12 15:07:56 +0400

    update :)

--- students/hoor_bakhit/experiments/2-3/sketch.js
+++ students/hoor_bakhit/experiments/2-3/sketch.js
@@ -4,23 +4,27 @@
 // Date 
 
 function setup(){
-
-	let canvas = createCanvas(windowWidth/4,windowWidth/4)
+let canvas = createCanvas(windowWidth/2,windowWidth/2)
 	
 	// Move the canvas so it's inside our <div id="sketch-holder">.
 	canvas.parent('sketch-holder');
 
-	background(34,155,215)
-
+	
+	
 
 }
 
 function draw(){
 
 
+fill(255);
+ ellipse(mouseX,mouseY,50,50);
+rect(mouseX,mouseY,100,100);
+
+
 }
 
-	background(random(255),155,215)
+	
 
 		// var string = x + "," + y
 		// text(string,x*step+20,y*step+20)
@@ -37,33 +41,5 @@ function draw(){
 //text (x,x*step,height/2)
 
 
-function draw(){
-
-	var numElements = 50
-
-var step= width/numElements
-textSize(10)
-textAlign(CENTER,CENTER)
-var colorstep = 255/numElements
-//console.log(step)
-
-//var x =0
-//var step =10
-
-// var x is the starting value
-
-for (var x = 0 ;x < numElements; x++){
-	for (var y = 0 ; y< numElements; y++){
-
-noStroke(255)
-strokeWeight(3)
-fill(x*colorstep,mouseX,y*colorstep)
-rect(x*step,y*step,step,step)
-fill(0)
-
-
-}
-}
-}
 
 
