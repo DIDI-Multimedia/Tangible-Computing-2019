a8ff9684dc337fe9aa2fc2018658c927daba8a1e
Ahsan <ahsanmurad92@gmail.com>
2019-09-13 20:05:32 +0400

    Update sketch.js

--- students/ahsan_murad/p5 template/sketch.js
+++ students/ahsan_murad/p5 template/sketch.js
@@ -13,10 +13,24 @@ function setup(){
 	canvas.parent('sketch-holder');
 	
 function draw(){
-	background(220, 0, 200);
+	background(100, 100, 100);
+	
+	stroke(255,204,0);
+	strokeWeight(1);
 	line(0,0,windowWidth/4,windowWidth/4)
+	line(25,0,windowWidth/4,windowWidth/4);
+	line(50,0,windowWidth/4,windowWidth/4);
+	line(75,0,windowWidth/4,windowWidth/4);
+	
+	fill(255, 0, 0);
+	stroke(0,255,0);
 	rect(200,200,25,75,5);
 	rectMode(CENTER);
+	
+	fill(220, 120, 50);
+	stroke('white');
 	quad(38,31,86,20,69,63,30,76);
+	quad(362,369,314,380,331,337,370,334);
+	
 
 }
