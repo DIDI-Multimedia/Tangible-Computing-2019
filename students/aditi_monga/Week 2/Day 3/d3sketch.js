2efa4f54774c2200813edabac0bf031597f65e38
Aditi <54794670+aditimonga@users.noreply.github.com>
2019-09-14 17:25:04 +0400

    s

--- students/aditi_monga/Week 2/Day 3/d3sketch.js
+++ students/aditi_monga/Week 2/Day 3/d3sketch.js
@@ -10,12 +10,12 @@ function draw(){
 
 	background(0)
 	stroke(255)
-	strokeWeight(mouseY/4)
+	strokeWeight(mouseY/30)
 
 	for (var x = 0; x <= width; x+=100) {
 		for (var y = 0; y <= height ; y+=100) {
-			fill(x, y, random(mouseY))
-			ellipse(x, y, 60, 60)
+			fill(x, y, random(mouseX))
+			rect(x, y, 60, 60)
 			
 		}
 	}
