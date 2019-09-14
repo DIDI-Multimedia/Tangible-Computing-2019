331ac13c751363bf7a2786875299f67c458cbc6b
Ahsan <ahsanmurad92@gmail.com>
2019-09-14 20:50:38 +0400

    day4sketch
    randomizer

--- students/ahsan_murad/p5 template/day 4/day4sketch.js
+++ students/ahsan_murad/p5 template/day 4/day4sketch.js
@@ -2,17 +2,18 @@
 // Ahsan Murad 
 // GitHub Username
 // Date 
-let c1X = 400;
-let c2Y = 400;
-let c3X = 400;
-let c3Y = 400;
-let c4Y = 400;
-let c5Y = 400;
+//let c1X = 400;
+//let c2Y = 400;
+//let c3X = 400;
+//let c3Y = 400;
+//let c4Y = 400;
+//let c5Y = 400;
 let c6X = 800; 
-let c7X = 400;
-let c7Y = 400;
+//let c7X = 400;
+//let c7Y = 400;
 let lineX = 400;
 let lineY = 400;
+let lineX2 = 800;
 function setup(){
 
 	let canvas = createCanvas(800,800)
@@ -38,8 +39,11 @@ function draw(){
 	lineX = lineX - 10;
 	lineY = lineY - 10;
 
-	line(400,400,800,400);
+	stroke(255, random(150), random(255));
+	strokeWeight(3);
+	line(400,400,800,lineX2);
 	line(400,400,400,800);
+	lineX2 = lineX2 - 10;
 
 	//fill(255,0,144); - awesome purple 
 	
@@ -52,42 +56,42 @@ function draw(){
 	noStroke();
 	
 	//middle to left = c1X
-	ellipse(c1X,400,50,50);
-	c1X = c1X - 1;
+	//ellipse(c1X,400,50,50);
+	//c1X = c1X - 1;
 
 	//middle to bottom left - c2Y
-	ellipse(c1X,c2Y,50,50);
-	c2Y = c2Y + 1;
+	//ellipse(c1X,c2Y,50,50);
+	//c2Y = c2Y + 1;
 
 	//middle to bottom right
-	ellipse(c3X,c3Y,50,50);
-	c3Y = c3Y + 1;
-	c3X = c3X + 1;
+	//ellipse(c3X,c3Y,50,50);
+	//c3Y = c3Y + 1;
+	//c3X = c3X + 1;
 
 	//middle to top
-	ellipse(400,c4Y,50,50);
-	c4Y = c4Y - 1;
+	//ellipse(400,c4Y,50,50);
+	//c4Y = c4Y - 1;
 
 	//middle to bottom
-	ellipse(400,c5Y,50,50);
-	c5Y = c5Y + 1;
+	//ellipse(400,c5Y,50,50);
+	//c5Y = c5Y + 1;
 
 	//middle to right
-	fill(57,255,20);
-	ellipse(c6X,400,50,50);
+	fill(255, random(150), random(255));
+	ellipse(c6X,400,random(0,50),random(0,50));
 	c6X--;
 
 	//middle to top right -> c7Y
-	ellipse(c7X,c7Y,50,50);
-	c7X++; //changing "c7X = c7X + 1" to simply entering "c7X++"" from now
-	c7Y--;
+	//ellipse(c7X,c7Y,50,50);
+	//c7X++; //changing "c7X = c7X + 1" to simply entering "c7X++"" from now
+	//c7Y--;
 
 	//function mousePressed(){
-	fill(252,102,0,200); // for mouseX ellipse change HERE! <---- currently set to Orange same as line
-	ellipse(mouseX,mouseY,50,50);
+	fill(random(255),random(255),random(255)); // for mouseX ellipse change HERE! <---- currently set to Orange same as line
+	ellipse(mouseX,mouseY,random(0,25),random(0,25));
 
 	for (var x=0,y=0; x<400,y<400;x=x+50,y=y+50){
-    fill(255,0,200);
+    fill(255,random(255),random(255));
     ellipse(x,y,25,25);
   }
 
