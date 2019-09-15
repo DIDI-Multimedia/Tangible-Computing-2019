bdef7143017a646a3ed9fc70d256055f360418b5
Aditi <54794670+aditimonga@users.noreply.github.com>
2019-09-15 13:49:48 +0400

    Create d5sketch.js

---
+++ students/aditi_monga/Week 2/Day 5/d5sketch.js
@@ -0,0 +1,13 @@
+function setup(){
+
+	let canvas = createCanvas(windowWidth,windowWidth/2)
+	canvas.parent('sketch-holder');
+	background(0)
+}
+
+
+function draw(){
+for (var i = 0; i < Things.length; i++) {
+	Things[i]
+}
+}
