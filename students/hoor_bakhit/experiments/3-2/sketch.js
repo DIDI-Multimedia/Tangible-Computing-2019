dad439f4f2a36f4bfe6471d337c623651ef637fd
Sayjel Patel <sayjel.v.patel@gmail.com>
2019-09-17 20:07:08 +0400

    updates - hoor's code - sp

--- students/hoor_bakhit/experiments/3-2/sketch.js
+++ students/hoor_bakhit/experiments/3-2/sketch.js
@@ -10,26 +10,29 @@ let y = 30
 
 function setup(){
 
-	let canvas = createCanvas(windowWidth/4,windowWidth/4)
+	let canvas = createCanvas(windowWidth/2,windowWidth/2)
 	
 	
 	// Move the canvas so it's inside our <div id="sketch-holder">.
 	canvas.parent('sketch-holder');
 	background(34,155,215)
-stroke(255); 
-  frameRate(50);
+  noStroke()
+  // stroke(255); 
+  // frameRate(50);
 
-    ball1 = new ball();
-	ball2 = new ball();
-	ball3 = new ball();
-	ball4 = new ball();
+  ball1 = getBall() // you can use a function to create the ball object and initialize it with random values 
+  ball2 = getBall()
+  ball3 = getBall()
+  ball4 = getBall()
+
+
 }
 
-let step =2
 
 function draw(){
-background(0)
-ball1.move();
+
+  background(0)
+  ball1.move();
   ball1.display();
   ball2.move();
   ball2.display();
@@ -37,17 +40,44 @@ ball1.move();
   ball3.display();
   ball4.move();
   ball4.display();
+
 }
 
 
-function ball(){
-move() {	
-	y = y + step;
-  if (y > height || y < 0) {
-    step *=-1; }
- }
-display() {
-  ellipse(50, y, 60, 60);
+function getBall(){
+
+  let ball ={
+    
+    size : random(10,50),
+    x: random(width), // random position width of div
+    y: random(height), // random position height of div
+    speed:random(15),
+    color:[random(255),random(255),random(255)], // this is a way of giving a random colour
+
+    move : function(){
+      //code here
+      this.y += this.speed // 'this' means the variables associate with this specific ball object
+
+      if (this.y > height || this.y < 0) {
+
+        this.speed *=-1.1; 
+
+      }
+      
+    },
+
+    display : function(){
+
+      //more code
+      fill(this.color)
+      ellipse(this.x, this.y, this.size, this.size);
+
+    }
+
+  }
+
+  return ball 
+
 }
-}
+
 
