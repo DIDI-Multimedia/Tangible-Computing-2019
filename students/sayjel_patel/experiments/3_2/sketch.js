6fbb2e64cb764b1f96af22b27c977f810ce2059a
Sayjel Patel <sayjel.v.patel@gmail.com>
2019-09-21 14:35:51 +0400

    updates

--- students/sayjel_patel/experiments/3_2/sketch.js
+++ students/sayjel_patel/experiments/3_2/sketch.js
@@ -1,33 +1,51 @@
-console.log('sketch.js')
+let i,j,k
 
-let i = 0
 
 function setup(){
 
   createCanvas(800,800);
-  stroke(255)
   noStroke()
-
+  rectMode(CENTER);
+  // initialize perlin noise values with random parameters
+  i = random()
+  j = random()
+  k = random()
+  noFill()
+  strokeWeight(3)
 }
 function draw(){
 
-  let numCol = 10
-  let numRow = 10
+  let numCol = 25
+  let numRow = 25
   let stepX = width / numCol // height of box 
   let stepY = height / numRow // width of box 
+  
    
   background(255)
 
-  for (var col = 0; col < numRow; col++){
+  for (var col = 0; col < numRow; col++ ){
     for (var row = 0; row < numCol; row++){
+     //fill(col/numCol*255,row/numRow*255,mouseX/width*255)
+     let r = noise(row/numRow+i)*255
+     let b = noise(col/numCol+j)*255
+     let g = noise(col/numCol+k)*255
+     boxWidth = noise(k*row/numRow)*stepX*3
+     boxHeight = noise(j*col/numCol)*stepY*6
+     stroke(r,g,b,150)
+     fill(r,g,b,100)
 
-      let r = noise(col/numCol + i)*255
-      let g = noise(row/numRow + i )*255
-      let b = noise(i)*255
-      fill(r,g,b)
-      rect(row*stepX,col*stepY,stepX,stepY) 
+     if (random()>0.9){
+      fill(255)
+      stroke(255)
+     }
+     
+
+     rect(row*stepX,col*stepY,boxWidth,boxHeight) 
+    
     }
   }
 
-  i += 0.01
+    j += 0.01
+    k += 0.0025
+    i += 0.005
 }
