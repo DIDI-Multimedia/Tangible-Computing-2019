1b4bba67e60f418370053c238db05e5513fa15c4
Aditi <54794670+aditimonga@users.noreply.github.com>
2019-09-22 10:50:49 +0400

    Update d6sketch.js

--- students/aditi_monga/Week 3/Day 6/d6sketch.js
+++ students/aditi_monga/Week 3/Day 6/d6sketch.js
@@ -1,41 +1,18 @@
-var a = 200
-var b = 200
-
 function setup(){
 
 	let canvas = createCanvas(windowWidth,windowWidth/2)
 	canvas.parent('sketch-holder');
-	background(0)
+	
 }
 
 
-function triRecursion(i){
-	i--
-	stroke(255)
-	strokeWeight(1)
-	fill(i, random(255))
-	triangle (0,100, i*10, i*20, i*30, mouseX, mouseY)
-	if (i > 0){
-		triRecursion(i)
-	}
 
-}
 function draw(){
 	background(0)
 
-	let i = 100
-	triRecursion(i)
+	for (var x = 10; x < ; x+=40) {
+		strokeWeight(3)
+		rect (x,x,20-random(-3,3),20)
+	}
 
-	// for (var i = 0; i < width; i++){
-	// 	for (var j = 0; j < width; j++){
-	// 		for (var = 0; k < height; k)
-	// 	}
-	// }
-	// for (var i = 50; i <= width && i <= height ; i+=20) {
-	// 	for (var j = 20; j <= width && i <= height ; j+=10) {
-	// 		for (var k = 30; k <= width && i <= height ; k+=30){
-	// 			triangle (0,100, i, k, j, mouseX, mouseY)
-	// 		}
-	// 	}
-	// }
 }
