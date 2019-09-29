175fefdffc78ead0b8da44a098bcb481475fd181
Aditi <54794670+aditimonga@users.noreply.github.com>
2019-09-29 20:09:35 +0400

    mic bc music

--- students/aditi_monga/Week 4/Day 1/d1sketch.js
+++ students/aditi_monga/Week 4/Day 1/d1sketch.js
@@ -1,59 +1,15 @@
-	var a = 0
-	var aMinus = -1
+var mic;
 
 function setup(){
-
-	let canvas = createCanvas(1400,800)
-	canvas.parent('sketch-holder');
+	createCanvas(windowWidth/2,windowWidth/2)
+	background(0)
+	mic = new p5.AudioIn()
+	mic.start()
 }
 
 function draw(){
-	background(255)
-	let pistilX = width/4
-	let pistilY = height/4
-	let numPetals = 10
-	let innerRadius = width/32
-	let outerRadius = width/16
-	let t = sin(a)
-	let color1 = [0,150*t,a]
-	let color2 = [155*t,0,30]
-	let rotation = PI/4
-	let speed = 1
-	let scale = 1
-	drawFlower(pistilX,pistilY,numPetals,innerRadius,outerRadius,color1,color2,rotation,speed,scale)
-	a+= 0.05
-
-}
-
-function drawFlower(pistilX,pistilY,numPetals,innerRadius,outerRadius,color1,color2,rotation,speed,scale){
-	
-	for (var pistilX = 0; pistilX < width; pistilX+= windowWidth/6) {
-		for (var pistilY = 0; pistilY < width; pistilY+= windowWidth/6){
-
-		}
-	
-
-	strokeWeight(4)
-	stroke(255)
-	push()
-
-	translate(pistilX,pistilY/8)
-	for(var angle=0; angle < TWO_PI; angle += TWO_PI/numPetals){
-		console.log(angle)
-		push()
-	rotate(angle)
-	fill(color2)
-	ellipse(0,outerRadius,outerRadius,outerRadius*3) //petal
-	pop()
-	
-	}
-
-
-	fill(color1)
-	ellipse(0,0,outerRadius,outerRadius) //outerpistil
-	fill(color2)
-	ellipse(0,0,innerRadius,innerRadius) //innerpistil
-	pop()
-}
-
+	var vol = mic.getLevel();
+	fill(255,10)
+	rectMode(CENTER)
+	rect(width/2, width/2, vol*500, vol*500)
 }
--- students/aditi_monga/Week 4/Day 4/d4sketch.js
+++ students/aditi_monga/Week 4/Day 4/d4sketch.js
@@ -1,24 +1,24 @@
-// var x = 100-random(-8,8)
-// var y = 200-random(-8,8)
+var mic;
+
 
 function setup(){
+	createCanvas(windowWidth/2,windowWidth/2)
 
-	let canvas = createCanvas(windowWidth,windowWidth/2)
-	canvas.parent('sketch-holder');
-	
-	
+	mic = new p5.AudioIn()
+	mic.start()
 }
 
-
 function draw(){
-
-	background(255)
-	strokeWeight(4)
-
-	for (var x = 10-random(-8,8); x <= width; x+=10) {
-		for (var y = 20-random(-8,8); y <= width; y+=20) {
-			triangle (x,x,y,y,x,y)
-		}
-	}
-
+  	background(100)
+	var vol = mic.getLevel();
+	
+    for (var x = 100*vol; x <= width; x+=100){
+      for (var y = 100*vol; y <=height; y+=100){
+        // fill(x*vol, y*random(200), vol*random(255))
+        strokeWeight(10)
+        stroke(vol*255)
+        rectMode(CENTER)
+        rect (x, y, vol*100,vol*100)
+      } 
+    }
 }
