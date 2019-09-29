ad087d76ba5d090eaea68f5d6b30ed2fdbcf41d5
Aysha 💗 <aishasuwaidii@outlook.com>
2019-09-29 22:04:59 +0400

    meow

--- students/aysha_alsuwaidi/Experiments/W3/Day 5/sketch.js
+++ students/aysha_alsuwaidi/Experiments/W3/Day 5/sketch.js
@@ -1,27 +1,23 @@
-var rectsize = 5
-var a = 3
+let a = 0.0;
+let s = 0.0;
 function setup() {
 let canvas = createCanvas(windowWidth/3, windowWidth/3)
   canvas.parent('sketch-holder');
-  background(255)
+  
 
 }
 
 function draw(){
-
-    for (var angle=0; angle < TWO_PI; angle+= TWO_PI/rectsize){
-      console.log(angle)
-
-      push()
-      translate(width/2,height/2)
-      rotate(angle)
-       stroke(random(255),random(255),random(255))
-       strokeWeight(20)
-      line(2,20,mouseX,mouseY)
+background(255)
+  
+ 
+  a = a + 0.02;
+  s = cos(a) * 2;
+  translate(width / 3, height / 3);
+      scale(s);
+      noFill()
       stroke(random(255),random(255),random(255))
-      line(0,2,(2+a)/2,a+20)
-      pop()
-    }
+      ellipse(0,0,50,50)
 
 }
 
