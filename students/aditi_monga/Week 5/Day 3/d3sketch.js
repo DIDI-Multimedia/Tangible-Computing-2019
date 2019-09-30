69914d95dd0c577f1af82a08eca476daf0532963
Aditi <54794670+aditimonga@users.noreply.github.com>
2019-09-30 11:43:40 +0400

    eh

---
+++ students/aditi_monga/Week 5/Day 3/d3sketch.js
@@ -0,0 +1,24 @@
+var mic;
+var x = 50
+var y = 50
+
+
+function setup(){
+	createCanvas(windowWidth/2,windowWidth/2)
+    canvas.parent('sketch-holder');
+
+	mic = new p5.AudioIn()
+	mic.start()
+}
+
+function draw(){
+  	background(0)
+	var vol = mic.getLevel();
+	
+    for (var x = 50; x <= width; x+=100){
+      for (var y = 50; y <=height; y+=100){
+        fill(x*vol, y*random(200), vol*random(255))
+        ellipse (x, y, vol*100,vol*100)
+      } 
+    }
+}
---
+++ students/aditi_monga/Week 5/Day 7/d7sketch.js
@@ -0,0 +1,29 @@
+// var angle = 180
+var mic;
+
+
+function setup(){
+  createCanvas(windowWidth/2,windowWidth/2)
+    // angleMode(DEGREES) 
+  mic = new p5.AudioIn()
+  mic.start()
+}
+
+function draw(){
+
+    background(0)
+  eyes ()
+  // smile()
+ 
+}
+
+function eyes(){
+    var vol = mic.getLevel();
+   ellipseMode(CENTER)
+  fill(255)
+    ellipse (100,100, width/4, vol*width/2)
+  ellipse ((width/2)+50, 100, width/4, vol*width/2)
+  fill(0)
+  ellipse(100,100,vol*200,vol*200)
+   ellipse((width/2)+50,100,vol*200,vol*200)
+}
