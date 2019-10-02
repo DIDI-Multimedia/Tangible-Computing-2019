0f9956d445ff134693db715e6147650ed6310e72
hoor bakhit <hoorkhamis2001@hotmail.com>
2019-10-02 12:33:07 +0400

    UPDATE :)

--- students/hoor_bakhit/experiments/5-3/sketch.js
+++ students/hoor_bakhit/experiments/5-3/sketch.js
@@ -1,118 +1,66 @@
 // Sketch Title - for MuW201 Tangible Computing, 2019
 // Firstname Lastname 
 // GitHub Username
-// Date 
- 
-let i,j,k
+//
 
+
+var xoff = 0;
 
 function setup(){
 
-  createCanvas(400,400);
-      let critter = {
-
-      name: "Me!", 
-      posX: width/2, 
-      posY: width/2,
-      rot: 0,  
-      size: 50, 
-      color: [215,155,214],
-      draw: drawAngryStickman 
-    
-    }
-
-    let critter02 = {
-
-      name: "KITT", 
-      posX: width/4, 
-      posY: width/4,
-      rot: 0,  
-      size: 50, 
-      color: [215,155,214],
-      draw: drawKitt 
-    
-    }
-
-    critter.draw()
-    critter02.draw()
-
-
-
-}
-
-
-
-// make your own creature using this code, share via whatsapp  
-
-
-    function drawAngryStickman(){
+  createCanvas(500,500);
      
-let c = this
+    color: [34, 43, 113]
+    }
 
-      push()
-    
+
+
+
+function draw(){
+  let c = this
+
+background(225)
+
+      
+   
       // fill in code here, draw a funny face
-
-      translate(c.posX,c.posY)
-      text(c.name, c.size,-c.size)
-      fill(c.color)
-      ellipse(0,0,c.size,c.size)
-
-fill(0)
-
-      ellipse(10,-10,c.size-40,c.size/16)
-      ellipse(-10,-10,c.size/5,c.size/16)
-
-fill(216,95,125)
-stroke(0)
-strokeWeight(2)
-  arc(0, 10, c.size-20, c.size-30, PI, TWO_PI);
-  
-fill(0)
-  strokeWeight(7)
-  line(0,120,0,26)
-line(1,20,c.size,c.size+10)
-line(1,20,-c.size,c.size+10)
-line(1,120,-c.size,c.size*3.5)
-line(1,120,c.size,c.size*3.5)
-
-
-      pop()
-
-
-    }
-
-     function drawKitt(){
-
-      let c = this
+var x = map(noise(xoff), 0, 1, 0, width);
+xoff += 0.01;
      
-  push()
-    
-  // fill in code here, draw a funny face
+   
+fill(c.color[0],c.color[1],c.color[2],30)
+      
 
-  translate(c.posX,c.posY)
-  fill(255)
-  text(c.name, c.size,-c.size)
-  fill(c.color)
-  ellipse(0,0,c.size,c.size)
-  fill(0)
-  rect(-(c.size-20),-c.size/5,c.size+10,c.size/10)
-  rect(-(c.size/c.size),-(c.size/12.5),4,2,6)
-  strokeWeight(2)
-  stroke(c.color)
-  noFill()
-  ellipse(0,0,c.size,c.size)
-  fill(0)
-  ellipse(0,15,15,15)
-  fill(c.color)
-  triangle(-12,-28,-18,-15,-2,-15,)
-  rect(-12,-40,0.4,15)
-  ellipse(-12,-43,4,4)
+       for (var i = 200;i > 0; i-=30){
+        ellipse(x,200,i,i)
+        
 
+      } 
+      }
 
-  pop()
-
-
+      function mousePressed() {
+  loop();
 }
+
+function mouseReleased() {
+  noLoop();
+
+      }// for (var r = c.size; r > 0 ; r-= 10){
+
+      //   ellipse(0,0,r,r)
+
+      // }
+
+
+      
+      
+     
+      
+     
+      
+    
+
+
+    
   
 
