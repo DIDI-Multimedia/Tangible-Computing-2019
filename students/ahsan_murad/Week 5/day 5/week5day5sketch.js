6db7ed2b5c0059584c47d55a378dea3c69017893
Ahsan <ahsanmurad92@gmail.com>
2019-10-07 00:43:27 +0400

    day 5 woohoo

--- students/ahsan_murad/Week 5/day 5/week5day5sketch.js
+++ students/ahsan_murad/Week 5/day 5/week5day5sketch.js
@@ -1,52 +1,72 @@
-var video;
-var vScale = 16;
-
-var particles = [];
-
-var slider;
-
-function Particle(x, y) {
-  this.x = x;
-  this.y = y;
-  this.r = random(4, 10);
   
-  this.update = function() {
-    this.x += random(-10, 10);
-    this.y += random(-10, 10);
+  function setup(){
 
-    this.x = constrain(this.x, 0, width);    
-    this.y = constrain(this.y, 0, height);    
-  }
+  let canvas = createCanvas(400,400);
+  canvas.parent('sketch-holder');
+  angleMode(DEGREES);
+
+}
+//For Future Coloring
+//"#1a0633" - purple
+//"#581845" - light purple
+//"#900C3F" - reddish marroon 
+//"#C70039" - red
+//"#FF5733" - orange
+//"#FFC30F" - yellow
+//237,34,93 - red/pinkish background rn for tingling - week 5 d2+d3+d4 
+
+
+function draw()
+{
+  background(255);
+  let h = hour();
+  let m = minute();
+  let s = second();
+
+   
+  strokeWeight(8);
+  stroke("0");
+  noFill();
+  ellipse(200,200,300,300);
   
-  this.show = function() {
-    noStroke();
-    var px = floor(this.x / vScale);
-    var py = floor(this.y / vScale);
-    var col = video.get(px, py);
-    //console.log(col);
-    fill(col[0], col[1], col[2], slider.value());
-    ellipse(this.x, this.y, this.r, this.r);    
-  }
+  strokeWeight(6);
+  stroke ("#FF5733");
+  noFill();
+  let end = map(s,0,60,0,360);
+  arc(200,200,300,300,0,end); 
   
+  noStroke();
+  fill ("0");
+  text(h + ':' + m + ':' + s, 175,200);
+
+fill(237,34,93);
+  noStroke();
+  var diameter = 25;
+  
+  for (var i = 0; i < width / diameter; i = i + 1) {
+    for (var j = 0; j < height / diameter; j = j + 1){
+    ellipse(
+      diameter / 2 + i * diameter,
+      diameter / 2 + j * diameter,
+      diameter * noise(m + i + j),
+      diameter * noise(m + i + j));
+}
 }
 
-function setup() {
-  createCanvas(640, 480);
-  pixelDensity(1);
-  video = createCapture(VIDEO);
-  video.size(width/vScale, height/vScale);
-  for (var i = 0; i < 100; i++) {
-    particles[i] = new Particle(random(width), random(height));
-  }
-  slider = createSlider(0, 255, 127);
-  background(51);
+translate(width/2,height/2);
+beginShape();
+  
+  for(let a=mouseX; a<TWO_PI; a+=0.05){
+    let r = random(100,150);
+    let x = r * cos(a);
+    let y = r * sin(a);
+    vertex(x,y);
 }
+endShape();
 
-function draw() {
-  //background(51);
-  video.loadPixels();
-  for(var i = 0; i < particles.length; i++) {
-    particles[i].update();
-    particles[i].show();
-  }
-}
+   
+   }
+
+
+  
+
