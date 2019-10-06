e1c426561d96d244a0ae451ae8c60ad9a485faa7
Aysha 💗 <aishasuwaidii@outlook.com>
2019-10-06 21:52:27 +0400

    .

--- students/aysha_alsuwaidi/Experiments/W4/Day 2/sketch.js
+++ students/aysha_alsuwaidi/Experiments/W4/Day 2/sketch.js
@@ -8,9 +8,4 @@ function setup() {
 
 
 function draw() {
-  background(255)
-  translate(width/2,height/2);
-  stroke(random(255),random(255),random(255))
-  strokeWeight(20);
-  line(100,100,5*2,10*3);
-}
+ 
