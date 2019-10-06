8aef4aac543cb2310c3f81a860c99a69e07313e1
Aditi <54794670+aditimonga@users.noreply.github.com>
2019-10-06 16:05:28 +0400

    wut

--- students/aditi_monga/Week 5/Day 2/d2sketch.js
+++ students/aditi_monga/Week 5/Day 2/d2sketch.js
@@ -1,8 +1,15 @@
 var mic;
+var song;
+
+function preload(){
+	song = loadSound('https://drive.google.com/file/d/183g550iL68tFs-JiQvGLsLjc7aDcfW_9/view?usp=sharing')
+}
 
 function setup(){
 	let canvas = createCanvas(windowWidth/2,windowWidth/2)
-		canvas.parent('sketch-holder');
+	canvas.parent('sketch-holder');
+
+	song.play();
 
 	mic = new p5.AudioIn()
 	mic.start()
@@ -12,6 +19,5 @@ function draw(){
   	background(0)
 	var vol = mic.getLevel();
 	fill(vol*255,vol*59,vol*200)
-	// rectMode(CENTER)
 	ellipse(width/2, width/2,600,600)
 }
