69445ad063c93590293efc2816677b5d71afb0b9
Rafif Al Hassen <ralhassen@gmail.com>
2019-10-06 21:49:58 +0400

    Updates - I'm really trying

--- students/rafif_alhassen/week5/p5 template 6/sketch.js
+++ students/rafif_alhassen/week5/p5 template 6/sketch.js
@@ -1,58 +1,41 @@
-// let capture;
-var video;
-
- var vScale = 16
+let coswave = [];
 
 function setup() {
-  createCanvas(640, 480);
-  pixelDensity(1);
-  video = createCapture(VIDEO);
-  video.size(width/vScale, height/vScale);
-  // video.size(width / vScale, height / vScale);
+  createCanvas(720, 360);
+  for (let i = 0; i < width; i++) {
+    let amount = map(i, 0, width, 0, PI);
+    coswave[i] = abs(cos(amount));
+  }
+  background(random(255),random(255),random(255));
+  // noLoop();
 
+  var i;
+  for (i= 0; i < 10; i++){
+    console.log(i);
+  }
 }
-  // capture = createCapture(VIDEO); 
-  // capture1 = createCapture(VIDEO);
-  // capture.size(320, 240);
-  // capture1.size(320, 240);
-  // //capture.hide();
 
 function draw() {
-  background(50);
-
-  video.loadPixels();
-  loadPixels();
-  for (var y = 0; y < video.height; y++){
-    for (var x = 0; x < video.width; x++){
-      var index = (x + y * video.width)*4;
-      var r = video.pixels[index+0];
-      var g = video.pixels[index+1];
-      var b = video.pixels[index+2];
-
-      // noStroke();
-      var bright = (r*g/b)/3;
-      fill(bright);
-      ellipse(x*vScale, y*vScale, vScale, vScale)
-
-      // var w = map(bright, 0, 255, 0, vScale);
-
-      // noStroke();
-      // fill(118,115,169);
-      // rect(RIGHT);
-      // rect(x*vScale, y*vScale, w, w);
-
-    }
+  let y1 = 0;
+  let y2 = height / 3;
+  for (let i = 0; i < width; i += 4) {
+    stroke(coswave[i] * 255);
+    line(i, y1+2, i, y2*4);
   }
 
-}
-  // image(capture, 0, 0, 320, 240);
-  // filter(THRESHOLD);
-  // color(random(255), random(255),random(255))
-  // image(capture1, 0, 0, 320, 240);
-  // filter(THRESHOLD);
-  // image(capture, 0, 0, 320, 240);
-  // filter(THRESHOLD);
-  // filter(INVERT);
+  y1 = y2;
+  y2 = y1 - y1;
+  for (let i = 0; i < width; i += 3) {
+    stroke((coswave[i] * 86) /6);
+    line(i, y1, i, y2);
+  }
 
-// source: https://p5js.org/examples/dom-video-capture.html
-// source: https://www.youtube.com/watch?v=rNqaw8LT2ZU
+  y1 = y2;
+  y2 = height;
+  for (let i = 0; i < width; i += 7) {
+    stroke((random(255),random(255),random(255)) - coswave[i] * 255);
+    line(i, y1, i, y2);
+  }
+}
+
+// source: https://p5js.org/examples/arrays-array.html
