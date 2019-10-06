4d520a7ef71f85b08b87eaf1449558abe31ec83f
Ahsan <ahsanmurad92@gmail.com>
2019-10-06 23:44:52 +0400

    Day 4
    "OMGOMG" - an actual clock

--- students/ahsan_murad/Week 5/day 4/week5day4sketch.js
+++ students/ahsan_murad/Week 5/day 4/week5day4sketch.js
@@ -1,8 +1,8 @@
-function setup(){
+  function setup(){
 
   let canvas = createCanvas(400,400);
   canvas.parent('sketch-holder');
-  //angleMode(DEGREES);
+  angleMode(DEGREES);
 
 }
 //For Future Coloring
@@ -12,47 +12,48 @@ function setup(){
 //"#C70039" - red
 //"#FF5733" - orange
 //"#FFC30F" - yellow
+//237,34,93 - red/pinkish background rn for tingling - week 5 d2+d3+d4 
 
 
 function draw()
 {
   background("#1a0633");
-  //translate(200, 200);
-  //rotate(-HALF_PI);
   let h = hour();
   let m = minute();
   let s = second();
-  
-  let ArcSec = map(s,0,60,0,360);
-  let ArcMin = map(m,0,60,0,360);
-  let ArcHr = map(h,0,24,0,360);
 
-  stroke(220);
-  noFill();
-  ellipse(200,200,300,300);
    
   strokeWeight(8);
   stroke("0");
   noFill();
-  arc(200,200,300,300,0,ArcSec);
-  //rotate(-90);
-  stroke("#FFC30F");
-  arc(200,200,180,180,0,ArcMin);
-  stroke ("#C70039");
-  arc(200,200,240,160,0,ArHr);
+  ellipse(200,200,300,300);
   
-  //strokeWeight(6);
-  //stroke ("#FF5733");
-  //noFill();
-  //let end = map(s,0,60,0,200);
-  //line(200,200,50,200,50,end); 
+  strokeWeight(6);
+  stroke ("#FF5733");
+  noFill();
+  let end = map(s,0,60,0,360);
+  arc(200,200,300,300,0,end); 
   
   noStroke();
   fill ("0");
   text(h + ':' + m + ':' + s, 175,200);
 
+fill(237,34,93);
+  noStroke();
+  var diameter = 25;
+  
+  for (var i = 0; i < width / diameter; i = i + 1) {
+    for (var j = 0; j < height / diameter; j = j + 1){
+    ellipse(
+      diameter / 2 + i * diameter,
+      diameter / 2 + j * diameter,
+      diameter * noise(m + i + j),
+      diameter * noise(m + i + j));
+}
+}
 
 
   }
+
   
 
