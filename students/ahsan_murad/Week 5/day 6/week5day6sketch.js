0f1b4a68d8371f0666c8aad041b66b2adec04784
Ahsan <ahsanmurad92@gmail.com>
2019-10-07 01:54:16 +0400

    wavyyy

--- students/ahsan_murad/Week 5/day 6/week5day6sketch.js
+++ students/ahsan_murad/Week 5/day 6/week5day6sketch.js
@@ -1,15 +1,62 @@
 
+var video;
+
+var vScale = 16;
+
 function setup() {
-
-  let canvas = createCanvas(windowWidth/3, windowWidth/3)
-  canvas.parent('sketch-holder');
-
-
+  
+  createCanvas(640, 480);
+  pixelDensity(1);
+  
+  video = createCapture(VIDEO);
+  video.size(width / vScale, height / vScale);
 }
 
 function draw() {
-  background(255);
-  translate(width/2,height/2);
+  background(51);
+  video.loadPixels();
+  
+fill(237,34,93);
+  noStroke();
+  var diameter = 25;
+  
+  for (var i = 0; i < width / diameter; i = i + 1) {
+    for (var j = 0; j < height / diameter; j = j + 1){
+    ellipse(
+      diameter / 2 + i * diameter,
+      diameter / 2 + j * diameter,
+      diameter * noise(frameCount/100 + i + j),
+      diameter * noise(frameCount/100 + i + j));
+}
+}
+  
+  for (var y = 0; y < video.height; y++) {
+    for (var x = 0; x < video.width; x++) {
+      var index = (video.width - x + 1 + (y*video.width)) * 4;
+      var r = video.pixels[index + 0];
+      var g = video.pixels[index + 1];
+      var b = video.pixels[index + 2];
+      
+      var bright = (r + g + b) * 1;
+
+      var threshold = 127;
+
+      if (bright > threshold) {
+        fill(0);
+  }  else{
+    fill (255);
+  }
+      
+      var w = map(bright, 0, 255, 0, vScale);
+      
+      noStroke();
+      fill(random(255),random(255),random(255));
+      rectMode(CENTER);
+      rect(x * vScale, y * vScale, w, w);
+    }
+  }
+
+   translate(width/2,height/2);
 
   fill(0);
 
@@ -23,9 +70,9 @@ function draw() {
 endShape();
 
 beginShape();
-fill(155,0,0);
-for (let a = 0; a < TWO_PI; a+=0.1){
-  let r = random(30,50);
+fill(237,34,93);
+for (let a = 0; a < TWO_PI; a+=1){
+  let r = random(0,100,50);
   let x = r * cos(a);
   let y = r * sin(a);
   vertex(x,y);
@@ -33,3 +80,4 @@ for (let a = 0; a < TWO_PI; a+=0.1){
 endShape();
    
    }
+
--- students/ahsan_murad/Week 5/day 7/week5day7sketch.js
+++ students/ahsan_murad/Week 5/day 7/week5day7sketch.js
@@ -1,46 +1,61 @@
+let yoff = 0.0; // 2nd dimension of perlin noise
+
+//For Future Coloring
+//"#1a0633" - purple
+//"#581845" - light purple
+//"#900C3F" - reddish marroon 
+//"#C70039" - red
+//"#FF5733" - orange
+//"#FFC30F" - yellow
+//237,34,93 - red/pinkish background rn for tingling - week 5 d2+d3+d4 
 
 function setup() {
-
-  let canvas = createCanvas(windowWidth/3, windowWidth/3)
-  canvas.parent('sketch-holder');
-
-
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(255);
-  translate(width/2,height/2);
+  background(51);
 
-  fill(0);
-
-  beginShape();
-  fill(255);
-  for(let a=mouseX; a<TWO_PI; a+=0.05){
-    let r = random(100,150);
-    let x = r * cos(a);
-    let y = r * sin(a);
-    vertex(x,y);
+  fill(237,34,93);
+  noStroke();
+  var diameter = 25;
+  
+  for (var i = 0; i < width / diameter; i = i + 1) {
+    for (var j = 0; j < height / diameter; j = j + 1){
+    ellipse(
+      diameter / 2 + i * diameter,
+      diameter / 2 + j * diameter,
+      diameter * noise(frameCount/100 + i + j),
+      diameter * noise(frameCount/100 + i + j));
 }
-endShape();
-
-  beginShape();
-  fill(255);
-  for(let a=mouseY; a<TWO_PI/8; a+=1.05){
-    let r = random(150);
-    let x = r * cos(a);
-    let y = r * sin(a);
-    vertex(x,y);
 }
-endShape();
 
+  fill(237,34,93);
+  // We are going to draw a polygon out of the wave points
   beginShape();
-  fill(155,0,0);
-  for (let a = 0; a < TWO_PI; a+=0.9){
-    let r = random(30,50);
-    let x = r * cos(a);
-    let y = r * sin(a);
-  vertex(x,y);
+
+  let xoff = 0; // Option #1: 2D Noise
+  // let xoff = yoff; // Option #2: 1D Noise
+
+  // Iterate over horizontal pixels
+  for (let x = 0; x <= width; x += 10) {
+    // Calculate a y value according to noise, map to
+
+    // Option #1: 2D Noise
+    let y = map(noise(xoff, yoff), 0, 1, random(0),399);
+
+    // Option #2: 1D Noise
+    // let y = map(noise(xoff), 0, 1, 200,300);
+
+    // Set the vertex
+    vertex(x, y);
+    // Increment x dimension for noise
+    xoff += 0.05;
+  }
+  // increment y dimension for noise
+  yoff += 0.01;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
 }
-endShape();
-   
-   }
+
