6488384bcdc35a31fcf86f484b440eac2c181cb5
Aysha 💗 <aishasuwaidii@outlook.com>
2019-10-06 22:33:36 +0400

    .

--- students/aysha_alsuwaidi/Experiments/W4/Day 2/sketch.js
+++ students/aysha_alsuwaidi/Experiments/W4/Day 2/sketch.js
@@ -1,4 +1,4 @@
-
+let x = 100
 function setup() {
   let canvas = createCanvas(windowWidth/3, windowWidth/3)
   canvas.parent('sketch-holder');
@@ -8,4 +8,16 @@ function setup() {
 
 
 function draw() {
- 
+
+ x=x-2;
+ if (x< 1){
+ 	x= height;
+ }
+
+stroke(255)
+line(2,width/2,x,x);
+stroke(255)
+line(10,width/6,x,x);
+stroke(255)
+line(width/10,100,x,x);
+}
