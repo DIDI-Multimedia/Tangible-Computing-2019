cfaf6a766141a78f1b133a9ca839d22a3008c3e4
Aysha 💗 <aishasuwaidii@outlook.com>
2019-10-06 23:02:01 +0400

    .

--- students/aysha_alsuwaidi/Experiments/W4/Day 5/sketch.js
+++ students/aysha_alsuwaidi/Experiments/W4/Day 5/sketch.js
@@ -1,25 +1,23 @@
-let a = 0.0;
-let s = 0.0;
+let x = 100
 function setup() {
-let canvas = createCanvas(windowWidth/3, windowWidth/3)
+  let canvas = createCanvas(windowWidth/3, windowWidth/3)
   canvas.parent('sketch-holder');
-  
 
+
+  }
+
+
+function draw() {
+
+ x=x-50;
+ if (x< 0.5){
+ 	x= width;
+ }
+
+stroke(0)
+line(2,width/2,mouseX,x);
+
+line(10,width/6,x,x);
+
+line(width/10,100,x,mouseY);
 }
-
-function draw(){
-background(255)
-  
- 
-  a = a + 0.02;
-  s = cos(a) * 2;
-  translate(width / 3, height / 3);
-      scale(s);
-      noFill()
-      stroke(random(255),random(255),random(255))
-      ellipse(0,0,50,50)
-
-}
-
-
-
--- students/aysha_alsuwaidi/Experiments/W4/Day 6/sketch.js
+++ students/aysha_alsuwaidi/Experiments/W4/Day 6/sketch.js
@@ -1,17 +1,23 @@
-
+let x = 0
 function setup() {
-let canvas = createCanvas(windowWidth/3, windowWidth/3)
+  let canvas = createCanvas(windowWidth/3, windowWidth/3)
   canvas.parent('sketch-holder');
-  background(255)
 
-}
 
-function draw(){
-
-      if (mouseIsPressed === true) {
-        stroke(200,random(255),random(255),random(255))
-        strokeWeight(70)
-        line(mouseX, mouseY, pmouseX, pmouseY);
   }
-}
 
+
+function draw() {
+
+ x=x-2;
+ if (x< 1){
+ 	x= height;
+ }
+
+stroke(255)
+line(2,width/2,x,x);
+stroke(255)
+line(10,width/6,x,x);
+stroke(255)
+line(width/10,100,x,x);
+}
