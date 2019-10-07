0a3d4e253bfaf9bdd7b36c3f114d11bb6df975a0
Noorio <nalfahim@icloud.com>
2019-10-07 11:28:42 +0400

    s

--- students/noor_alfahim/Week6Ex/Day 4/sketch.js
+++ students/noor_alfahim/Week6Ex/Day 4/sketch.js
@@ -1,62 +1,69 @@
-// Sketch Title - for MuW201 Tangible Computing, 2019
-// Firstname Lastname 
-// GitHub Username
-// Date 
-
-function setup(){
-
-	createCanvas(1000,1000);
-	background(255)
-
-	let flower01 = createFlower()
-	let flower02 = createFlower()
-	drawFlower (flower01)
-	push()
-	translate(width/2,height/2)
-	drawFlower (flower02)
-	pop()
 
 
-}
-	
-function createFlower(){
+function setup (){
+  createCanvas (400,400)
+  background(0)
 
-	let flower = {}
-	flower.posX = 300
-	flower.posY = 300 
-	flower.numPetals = 20
-	flower.innerRadius = 100
-	flower.outerRadius = 200
-	flower.color1 = [100,150,200]
-	flower.color2 = [200,150,100]
-	flower.rotation = 30
-	flower.speed = 1
-	flower.scale = 1
-	
-	return flower
 
+
+  let line = {
+
+    x1: width/2,
+    y1: 0, 
+    x2: width/2,
+    y2: height,
+
+  }
+
+
+
+  let numLines = 20
+
+  drawLineRecursion (line,numLines)
 
 }
 
-function drawFlower(flower){
-
-	fill(flower.color1)
-
-	for(var angle = 0; angle < TWO_PI; angle += TWO_PI/flower.numPetals){
-
-		console.log(angle)
-
-		push()
-		translate(flower.posX,flower.posY)
-		rotate(angle)
-		ellipse(0,flower.innerRadius,(flower.outerRadius-flower.innerRadius)/2,flower.outerRadius-flower.innerRadius)
-		pop()
-
-	}
 
 
-fill(flower.color2)
-ellipse(flower.posX, flower.posY, flower.innerRadius, flower.innerRadius)
+
+function drawLineRecursion(l,numLines){
+
+  stroke(random(255),random(255),random(255),50)
+  strokeWeight (numLines*10)
+
+  let mid = (l.y1 +l.y2)/2
+  line(l.x1,l.y1,l.x2,l.y2)
+
+
+    numLines = numLines - 1
+
+    let lineA = {
+
+    	x1: l.x1,
+    	y1: mid,
+    	x2: l.x2,
+    	y2: l.y2,
+    }
+
+
+    let lineB = {
+
+    	x1: l.x1,
+    	y1: l.y1,
+    	x2: l.x2,
+    	y2: mid,
+    }
+
+    console.log(lineA)
+
+
+    if (numLines > 0){
+      drawLineRecursion (l,numLines)
+      console.log(numLines)
+
+    }
+
+  // drawLineRecursion()
 
 }
 
