76f4d328508b1c2d0fb91a64928093b8fc1856ab
Noorio <nalfahim@icloud.com>
2019-10-07 11:24:36 +0400

    n

--- students/noor_alfahim/Week6Ex/Day 3/sketch.js
+++ students/noor_alfahim/Week6Ex/Day 3/sketch.js
@@ -28,12 +28,35 @@ function setup (){
 
 function drawLineRecursion(l,numLines){
 
-  stroke(random(255),random(255),random(255))
+  stroke(random(255),random(255),random(255),50)
   strokeWeight (numLines*10)
+
+  let mid = (l.y1 +l.y2)/2
   line(l.x1,l.y1,l.x2,l.y2)
 
 
     numLines = numLines - 1
+
+    let lineA = {
+
+    	x1: l.x1,
+    	y1: mid,
+    	x2: l.x2,
+    	y2: l.y2,
+    }
+
+
+    let lineB = {
+
+    	x1: l.x1,
+    	y1: l.y1,
+    	x2: l.x2,
+    	y2: mid,
+    }
+
+    console.log(lineA)
+
+
     if (numLines > 0){
       drawLineRecursion (l,numLines)
       console.log(numLines)
