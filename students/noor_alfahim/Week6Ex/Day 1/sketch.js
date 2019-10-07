b07409976fe36cbc1a810b0bd75cf80e9a8fcbd5
Noorio <nalfahim@icloud.com>
2019-10-07 10:52:37 +0400

    h

--- students/noor_alfahim/Week6Ex/Day 1/sketch.js
+++ students/noor_alfahim/Week6Ex/Day 1/sketch.js
@@ -3,16 +3,43 @@
 function setup (){
   createCanvas (400,400)
   background(0)
-  drawLineRecursion ()
+
+
+
+  let line = {
+
+    x1: width/2,
+    y1: 0, 
+    x2: width/2,
+    y2: height/2,
+
+  }
+
+
+
+  let numLines = 10
+
+  drawLineRecursion (line,numLines)
 
 }
 
 
 
 
-function drawLineRecursion(){
+
+
+
+
+
+
+
+
+
+
+function drawLineRecursion(line,numLines){
+
+
+
   drawLineRecursion()
-
-
 
 }
