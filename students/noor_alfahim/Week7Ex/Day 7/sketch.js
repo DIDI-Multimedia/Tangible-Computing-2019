cdc328c7c4e9d60d94872006db4d1bb2f8ea03cb
Noorio <nalfahim@icloud.com>
2019-10-14 12:04:08 +0400

    bh

--- students/noor_alfahim/Week7Ex/Day 7/sketch.js
+++ students/noor_alfahim/Week7Ex/Day 7/sketch.js
@@ -1,63 +1,45 @@
-// Sketch Title - for MuW201 Tangible Computing, 2019
-// Firstname Lastname 
-// GitHub Username
-// Date 
-
-function setup(){
-
-	createCanvas(1000,1000);
-	background(255)
-
-	let flower01 = createFlower()
-	let flower02 = createFlower()
-	drawFlower (flower01)
-	push()
-	translate(width/2,height/2)
-	drawFlower (flower02)
-	pop()
 
 
-}
-	
-function createFlower(){
-
-	let flower = {}
-	flower.posX = 300
-	flower.posY = 300 
-	flower.numPetals = 20
-	flower.innerRadius = 100
-	flower.outerRadius = 200
-	flower.color1 = [100,150,200]
-	flower.color2 = [200,150,100]
-	flower.rotation = 30
-	flower.speed = 1
-	flower.scale = 1
-	
-	return flower
+function setup (){
+  createCanvas (400,400)
+  background(0)
 
 
-}
 
-function drawFlower(flower){
+  let line = {
 
-	fill(flower.color1)
+    x1: width/2,
+    y1: 0, 
+    x2: width/2,
+    y2: height/2,
 
-	for(var angle = 0; angle < TWO_PI; angle += TWO_PI/flower.numPetals){
-
-		console.log(angle)
-
-		push()
-		translate(flower.posX,flower.posY)
-		rotate(angle)
-		ellipse(0,flower.innerRadius,(flower.outerRadius-flower.innerRadius)/2,flower.outerRadius-flower.innerRadius)
-		pop()
-
-	}
+  }
 
 
-fill(flower.color2)
-ellipse(flower.posX, flower.posY, flower.innerRadius, flower.innerRadius)
+
+  let numLines = 10
+
+  drawLineRecursion (line,numLines)
 
 }
 
 
+
+
+function drawLineRecursion(l,numLines){
+
+  stroke(random(255),random(255),random(255))
+  strokeWeight (numLines*25)
+  line(l.x1,l.y1,l.x2,l.y2)
+
+
+    numLines = numLines - 1
+    if (numLines > 0){
+      drawLineRecursion (l,numLines)
+      console.log(numLines)
+
+    }
+
+  // drawLineRecursion()
+
+}
