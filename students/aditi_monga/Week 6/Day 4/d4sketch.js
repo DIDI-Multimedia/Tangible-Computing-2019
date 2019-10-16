096350e060a68b1cbc7fef72ecbd2ae8db6e6752
Aditi <54794670+aditimonga@users.noreply.github.com>
2019-10-16 14:54:41 +0400

    oh i forgot to push

--- students/aditi_monga/Week 6/Day 4/d4sketch.js
+++ students/aditi_monga/Week 6/Day 4/d4sketch.js
@@ -1,29 +1,22 @@
-var mic;
+let mic
 
-
-function setup(){
-let canvas = createCanvas(windowWidth/2,windowWidth/2)
-    canvas.parent('sketch-holder')
-
-    angleMode(DEGREES)
-
-	mic = new p5.AudioIn()
-	mic.start()
+function setup() {
+ let canvas = createCanvas(windowWidth/2,windowWidth/2)
+  canvas.parent('sketch-holder')
+  mic = new p5.AudioIn();
+  mic.start();
+  fft = new p5.FFT();
+  fft.setInput(mic);
 }
 
-function draw(){
- 
-	var vol = mic.getLevel();
-
-  background(10,100,vol*1000)
-
-  push()
-  translate (width/2,width/2)
-  scale(vol*8)
-  rotate(vol*360)
-  rectMode(CENTER)
-  noStroke()
-  fill(vol*1000,100,10)
-  rect(0,0,100,50)
-  pop()
+function draw() {
+  background(200);
+  let vol = mic.getLevel();
+  let spectrum =  fft.analyze();
+  // console.log(spectrum)
+  
+  for (var i = 0; i < spectrum.length; i++){
+  stroke(random(255))      
+  rect (i, 0, 1, spectrum[i])
+  }
 }
