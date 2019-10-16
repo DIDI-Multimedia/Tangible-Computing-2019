51ce855df14aed02188ad093564ed09fee87c4e8
Rafif Al Hassen <ralhassen@gmail.com>
2019-10-16 12:54:50 +0400

    i really am trying

--- students/rafif_alhassen/week6/p5 template 5/sketch.js
+++ students/rafif_alhassen/week6/p5 template 5/sketch.js
@@ -1,8 +1,22 @@
 let x = 200;
 let y = 200;
 
+let fr = 30;
+
 let thoughts = ["i am hungry", "i am stressed out", "who is texting me", "what should i have for lunch", "i am tired", "i am overwhelmed", "i am struggling"]
+
+
+let firstPart = ["I am", "Who is","She is","He is","We are","They are"]
+
+let secondPart = [" hungry", " late for class", " eye infection, miss"]
+
+
 let freeThoughts = []
+
+// var lastPrint;
+// var v = 0;
+
+// thoughts.lifespan = 2;
 
 function setup() {
 
@@ -11,16 +25,32 @@ function setup() {
   // img = loadImage("assets/final.png");
   // image(img, mouseX, mouseY);
   // stroke(0)
+  frameRate(fr);
   background(0);
+  console.log(adjectives)
+  lifeSpan = 0.2;
+  // lastPrint = millis () - 3000;
   // fill(255)
 
 }
 
 function draw() {
 
+  // var timeElapsed = millis( - lastPrint;
+
+  //   if (timeElapsed > 3000) {
+  //     v++;
+  //     console.log(v);
+  //     last Print = millis(); 
+
   background(0);
-  fill(random(255),random(255),random(255));
+  drainBrain()
+  // fill(random(255),random(255),random(255));
   stroke(0);
+  frameRate(fr);
+  fill(0);
+  textFont('Times New Roman');
+  textSize(20);
  
   for (var i = 0; i < freeThoughts.length; i++){
     let tht = freeThoughts[i]
@@ -44,27 +74,82 @@ function draw() {
   //   ellipse(mouseX, mouseY, 60, 60);
   }
 
-function move(tht){
-
-  tht.x += random(-5, 5);
-  tht.y += random(-5, 5);
-
-}
+  var r = 80
+  var g = 120
+  var b = 255
 
 function mousePressed(){
-  console.log('mouse pressed ')
+  console.log('mouse pressed')
   // creates a new thought 
 
   let tht = {}
   tht.x = width/2 
   tht.y = height/2 
-  tht.txt = thoughts[Math.floor(random(thoughts.length))] // get random thought
-
+  tht.txt = getRandomThought(firstPart) + " " + getRandomThought(adjectives)
+  // tht.lifespan = 2;
+  frameRate(fr);
   freeThoughts.push(tht)
-
+  this.lifeSpan--
+  fill(random(255), random(255), random(255))
+  r = random(50+20,r);
+  g = random(30+10,g);
+  b = random(70+35,b);
 
 
 }
+
+function drainBrain(){
+
+  console.log('brain')
+  noStroke()
+  fill(255);
+  fill(r,g,b);
+  frameRate(fr);
+  ellipse(width/2,height/2,300,500)
+  // ellipse(500,00);
+}
+
+
+function move(tht){
+
+  tht.x += random(-5, 5);
+  tht.y += random(-5, 5);
+  frameRate(fr);
+
+}
+
+// function mousePressed(){
+//   console.log('mouse pressed')
+//   // creates a new thought 
+
+//   let tht = {}
+//   tht.x = width/2 
+//   tht.y = height/2 
+//   tht.txt = getRandomThought(firstPart) + " " + getRandomThought(adjectives)
+//   // tht.lifespan = 2;
+//   frameRate(fr);
+//   freeThoughts.push(tht)
+//   fill(random(255), random(255), random(255)
+//   ellipse();
+
+// }
+
+
+function getRandomThought(wordList){
+
+  let string =  wordList[Math.floor(random(wordList.length))] // get random thought
+
+  return string 
+   frameRate(fr);
+
+}
+
+
+// add life span
+// add color to the background? changes? 
+// add pronouns 
+// as one thought appears the other disappears
+// font & font size changes
 
 
 // source: https://www.youtube.com/watch?v=TaluaAD9MKA&list=PLRqwX-V7Uu6Zy51Q-x9tMWIv9cueOFTFA&t=0s
