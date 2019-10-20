e0878f3a140269fe2146a720f8bcabf21358e0c1
Aditi <54794670+aditimonga@users.noreply.github.com>
2019-10-20 21:13:15 +0400

    im excited

--- students/aditi_monga/Week 7/Day 2/d2sketch.js
+++ students/aditi_monga/Week 7/Day 2/d2sketch.js
@@ -1,11 +1,13 @@
+// from https://p5js.org/reference/#/p5.PeakDetect
+
 let mic
 let fft
 let peakDetect
-let ellipseWidth = 10
+let ellipseWidth = 40
 
 function setup() {
 
- let canvas = createCanvas(windowWidth/2,windowWidth/2)
+ let canvas = createCanvas(windowWidth/2,windowHeight/2)
   canvas.parent('sketch-holder')
 
   mic = new p5.AudioIn()
@@ -13,22 +15,31 @@ function setup() {
 
   fft = new p5.FFT()
   fft.setInput(mic)
-  peakDetect = new p5.PeakDetect()
+  peakDetect = new p5.PeakDetect
 
 }
 
 function draw() {
-  background(200);
+  // background(200);
   let vol = mic.getLevel()
   fft.analyze();
   peakDetect.update(fft);
   // console.log(spectrum)
   
-  if ( peakDetect.isDetected ) {
+  if (peakDetect.isDetected) {
   	ellipseWidth = 50;
+    background (random(255),random(255),random(255))
+    fill(random(255),random(255),random(255))
   } else {
   	ellipseWidth *= 0.95;
+    // background (0)
   }
 
-  ellipse(width/2, height/2, ellipseWidth, ellipseWidth);
+for (var x = 0; x < width+300; x+=50){
+  for (var y = 0; y < height+300; y +=50){
+  stroke(random(255),random(255),random(255))
+  rectMode (CENTER)
+  rect(x, y, ellipseWidth, ellipseWidth);
+}
+}
 }
